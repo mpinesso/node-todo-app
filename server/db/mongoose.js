78a80e14a862ffa3c3daef2b597507c3b26bd453
Marco Pinesso <pinesso.marco@gmail.com>
2017-08-19 19:32:43 +0200

    heroku fix db connection

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,10 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI, {
+
+console.log(process.env);
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', {
   useMongoClient: true
 });
 
