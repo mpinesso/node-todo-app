5d5d4d222afa671d496086848722d5fa7d3da4cd
Marco Pinesso <pinesso.marco@gmail.com>
2017-08-22 15:09:12 +0200

    Update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -6,12 +6,19 @@ console.log('env *****', env);
   * Questo codice va bene nei sistemi MAC e LINUX: export NODE_ENV=test
   * Questo codice invece va per il sistema WINDOWS: SET \"NDOE_ENV=test\"
   */
+if(env === 'development' || env === 'test'){
+  var config = require('./config.json');
+  var envConfig = config[env];
 
-// in questa maniera posso usare va
-if(env === 'development'){
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-}else if (env === 'test'){
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+  Object.keys(envConfig).forEach((key) => {
+    process.env[key] = envConfig[key];
+  });
 }
+
+// if(env === 'development'){
+//   process.env.PORT = 3000;
+//   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+// }else if (env === 'test'){
+//   process.env.PORT = 3000;
+//   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+// }
